3c3cbe1d4bae372192cde8ffc995266b465df4f7
fastsly <szalai.laszlo.istvan.psiho@gmail.com>
2020-11-02 09:47:36 +0200

    list updated

--- src/beneficiari.js
+++ src/beneficiari.js
@@ -1,26 +1,170 @@
-const benef = [
+const benef = [    
     {
-        name:'Putnoki Lorand',
+        name:'Abrudan Ioana',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Barta Emese',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Berki Melinda',
         cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Bot Mariana',
+        cosemnat:'Gergely Livia'
     },
     {
         name:'Buroi Alexandra',
         cosemnat:'Szalai Laszlo'
     },
     {
-        name:'Covaci Emilia',
+        name:'Buza Laura',
         cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Chirila Octavian',
+        cosemnat:'Gergely Livia'
     },
     {
         name:'Cordovan Loredana',
         cosemnat:'Szalai Laszlo'
     },
     {
+        name:'Cotrau Iuliana',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Covaci Csaba',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Covaci Emilia',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Covaci Rodica',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'David Iuliana',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Dragos Anamaria',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Duta Gabriela',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Eiben Hardy',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Farkas Eniko',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Fekete Florica',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Haikai Ileana',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Indig Daniela',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Iovan Florian',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Jecan Norbert',
+        cosemnat:'Gergely Livia'
+    },
+    {
         name:'Kanalas Monika',
         cosemnat:'Szalai Laszlo'
     },
     {
+        name:'Kozak Ianos',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Lazar Bogdan',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Magori Gabriela',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Mascas Lucian',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Murvai Alexandru',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Mustea Tibor',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Pacurar Raluca',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Popa Etelca',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Popa Mirela',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Putnoki Lorand',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Racz Monika',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Stoian Sorin',
+        cosemnat:'Gergely Livia'
+    },
+    {
         name:'Simon Angela',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Tenkei Tamas',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Ungur Ioan',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Varga Linda',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Szalai Laszlo',
+        cosemnat:'Szalai Laszlo'
+    },
+    {
+        name:'Gergely Livia',
+        cosemnat:'Gergely Livia'
+    },
+    {
+        name:'Zambo Gyorgy',
         cosemnat:'Szalai Laszlo'
     }
 ];
