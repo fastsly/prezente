f0bf1f58f61cdec74082b975bc442ce048081f78
fastsly <szalai.laszlo.istvan.psiho@gmail.com>
2020-11-09 13:39:54 +0200

    added datepicker

--- src/App.js
+++ src/App.js
@@ -1,70 +1,76 @@
-import React from 'react';
+import React,{useState} from 'react';
+import DatePicker from "react-datepicker";
+import CalendarContainer from "react-datepicker";
 import './App.css';
 import benef from './beneficiari.js'
 import BeneficiariList from './BeneficiariList'
 import Submit from './Submit'
+import "react-datepicker/dist/react-datepicker.css";
 
-class App extends React.Component {
-  constructor (){
-    super();
-    this.state={
-      temp : '',
-      name : '',
-      prezente:[]
-    }
-  }
-
-  onSubmit = () =>{
+function App () {
+  // constructor (){
+  //   super();
+  //   this.state={
+  //     temp : '',
+  //     name : '',
+  //     prezente:[]
+  //   }
+  // }
+  const [prezentaDate, setPrezentaDate] = useState(new Date());
+  const [temp, setTemp] = useState('')
+const [name, setName] = useState ('')
+const [prezente,setPrezente]= useState([])
+  const onSubmit = () =>{
     //i send data to server
-    fetch("http://frozen-basin-35628.herokuapp.com/daily", {
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(
-      this.state.prezente
-    ),
-    }).then((res, req) =>{
-      alert('Am reusit'+res.body)
-    })
-    .catch(err => alert("a fost o eroare "+err))
-    //console.log('the temp of '+ this.state.name+' is '+this.state.temp)
+    // fetch("http://frozen-basin-35628.herokuapp.com/daily", {
+    // method: "post",
+    // headers: { "Content-Type": "application/json" },
+    // body: JSON.stringify(
+    //   this.state.prezente
+    // ),
+    // }).then((res, req) =>{
+    //   alert('Am reusit'+res.body)
+    // })
+    // .catch(err => alert("a fost o eroare "+err))
+    console.log('the temp of '+ name+' is '+temp+' and date is'+ prezentaDate)
   }
 
-  setTemp = (event) =>{
-    this.setState({temp: event.target.value})
-  }
+  // setTemp = (event) =>{
+  //   this.setState({temp: event.target.value})
+  // }
 
-  onCheck = (event) =>{
+  const onCheck = (event) =>{
     //this.setState({name: event.target.value})
-    const date = new Date()
-    let temp = this.state.prezente
+    
+    let temp = prezente
     if (event.target.checked){
       
       benef.forEach(user =>{
         if (user.name === event.target.id){
-          temp.push({name:event.target.id, date, cosemnat: user.cosemnat})
+          temp.push({name:event.target.id, prezentaDate, cosemnat: user.cosemnat})
         }
       })
-      this.setState(temp)
+      setPrezente(temp)
     } else {
-      this.state.prezente.forEach((benef, i) => {
+      prezente.forEach((benef, i) => {
         if (benef.name === event.target.id){
           temp.splice(i,1)
-          this.setState(temp)
+          setPrezente(temp)
         }
       })
     }
-    console.log(this.state.prezente)
+    console.log(prezente)
   }
   
-  render(){
     return (
       <div className="App">
         <h1>Prezente</h1>
-        <BeneficiariList benef= {benef} onSelect = {this.onSelect} onCheck={this.onCheck}/>
-        <Submit onSubmit = {this.onSubmit}/>
+         <DatePicker selected={prezentaDate} onChange={date => setPrezentaDate(date)}  />
+        <BeneficiariList benef= {benef} onCheck={onCheck} />
+        <Submit onSubmit = {onSubmit}/>
       </div>
     );
-  }
+  
 }
 
 export default App;
